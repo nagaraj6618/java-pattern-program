b77ef1f9a6e3d6c94c9dff7c772e5af5862f6ff0
NAGARAJ S <nagaraj516700@gmail.com>
2024-06-29 11:54:55 +0530

    Updated formula

--- pattern5.java
+++ pattern5.java
@@ -2,38 +2,38 @@ public class pattern5 {
    public static void main(String[] args) {
     
       int n = 5;
-      int m=0;
+     
       for(int i=0;i<n;i++){
          
          for(int j=n;j>i;j--){
             System.out.print(" ");
          }
          
-         for(int k=0;k<i+1+m;k++){
+         for(int k=0;k<2*i+1;k++){
             System.out.print("*");
          }
-         m++;
+
          for(int j=n;j>i;j--){
             System.out.print(" ");
          }
          System.out.println("");
       }
-      m=0;
+
       for(int i=0;i<n;i++){
          for(int j=n;j>i;j--){
             System.out.print(" ");
          }
 
-         for(int k=0;k<i+1+m;k++){
+         for(int k=0;k<2*i+1;k++){
             System.out.print(k+1);
          }
-         m++;
+
          for(int j=n;j>i;j--){
             System.out.print(" ");
          }
          System.out.println("");
       }
-      m=0;
+
 
       for(int i=0;i<n;i++){
          for(int j=n;j>i;j--){
@@ -41,10 +41,10 @@ public class pattern5 {
             System.out.print(" ");
          }
 
-         for(int k=0;k<i+1+m;k++){
+         for(int k=0;k<2*i+1;k++){
             System.out.print(2*n-k-1);
          }
-         m++;
+
          for(int j=n;j>i;j--){
          
             System.out.print(" ");
