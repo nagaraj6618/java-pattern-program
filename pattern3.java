a050cdce78a3ae2c878c22546c1626ea0b1536ae
NAGARAJ S <nagaraj516700@gmail.com>
2024-06-29 10:21:54 +0530

    Right angle triangle with numbers completed

---
+++ pattern3.java
@@ -0,0 +1,29 @@
+/*
+   Right angle triangle with numbers;
+   
+   1
+   1 2
+   1 2 3
+   1 2 3 4
+
+ */
+import java.util.Scanner;
+public class pattern3 {
+   public static void main(String[] ags){
+      try{
+         
+         Scanner sc  = new Scanner(System.in);
+         int n = sc.nextInt();
+         sc.close();
+         for (int i = 0; i<n; i++){
+            for (int j=0;j<i+1;j++){
+               System.out.print(j+1+" ");
+            }
+            System.out.println("");
+         }
+      }
+      catch(Exception e){
+         e.printStackTrace();
+      }
+   }
+}
