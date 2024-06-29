4ea1984cb129c6d2213edbd164556593ce65eebe
NAGARAJ S <nagaraj516700@gmail.com>
2024-06-29 18:40:33 +0530

    Inverse Pyramid created

--- pattern6.java
+++ pattern6.java
@@ -10,6 +10,8 @@
 public class pattern6 {
    public static void main(String []args){
       int n = 5;
+
+      
       for(int i=0;i<n;i++){
 
          for(int j=0;j<i+1;j++){
@@ -23,6 +25,38 @@ public class pattern6 {
             System.out.print(" ");
          }
 
+         System.out.println("");
+      }
+
+      for(int i=0;i<n;i++){
+      
+         for(int j=0;j<i+1;j++){
+            System.out.print(" ");
+         }
+
+         for(int j=0;j<2*(n-i)-1;j++){
+            System.out.print(j+1);
+         }
+
+         for(int j=0;j<i+1;j++){
+            System.out.print(" ");
+         }
+         System.out.print("\n");
+      }
+
+      for(int i=0;i<n;i++){
+
+         for(int j=0;j<i+1;j++){
+            System.out.print(" ");
+         }
+
+         for(int j=0;j<2*(n-i)-1;j++){
+            System.out.print(n-i);
+         }
+
+         for(int j=0;j<i-1;j++){
+            System.out.print(" ");
+         }
          System.out.println("");
       }
 
