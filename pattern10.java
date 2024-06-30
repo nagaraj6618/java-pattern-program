3bc4a2161c022a0d176da90dd1b9ac9d7b33ff95
NAGARAJ S <nagaraj516700@gmail.com>
2024-06-30 17:47:26 +0530

    Half Diamond Star Pattern completed

---
+++ pattern10.java
@@ -0,0 +1,20 @@
+public class pattern10 {
+   public static void main(String [] args){
+      int n=5;
+      for(int i=0;i<n;i++){
+
+         for(int j=0;j<i+1;j++){
+            System.out.print(j+1);
+         }
+
+         for(int k=0;k<2*(n-i-1);k++){
+            System.out.print(" ");
+         }
+         for(int j=i+1;j>0;j--){
+            System.out.print(j);
+         }
+
+         System.out.print("\n");
+      }
+   }
+}
