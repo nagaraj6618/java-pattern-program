c6a3bc771ae694779ee93b5bcacf85cb260e1b03
NAGARAJ S <nagaraj516700@gmail.com>
2024-06-30 17:58:58 +0530

    Alphabet Pattern Created

---
+++ AlphabetPattern/pattern1.java
@@ -0,0 +1,25 @@
+/*
+   A
+   A B
+   A B C
+   A B C D
+   A B C D E
+   A B C D E F
+ */
+
+public class pattern1 {
+   
+   public static void main(String []args){
+
+      int n = 5;
+
+      for(int i=0;i<n;i++){
+
+         for(int j=0;j<i+1;j++){
+            System.out.print((char)(65+j) + " ");
+            
+         }
+         System.out.print("\n");
+      }
+   }
+}
