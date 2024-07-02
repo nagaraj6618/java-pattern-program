4ea080a07f00aa73fbb2062ca0ad00ac9e67ce0e
NAGARAJ S <nagaraj516700@gmail.com>
2024-07-02 11:30:55 +0530

    Pattern Added

---
+++ pattern11.java
@@ -0,0 +1,37 @@
+public class pattern11 {
+   public static void main(String[]args){
+      int n = 5;
+
+      for(int i=0;i<n;i++){
+         for(int j=0;j<n-i;j++){
+            System.out.print("*");
+         }
+         for(int k =0;k<2*i;k++){
+            System.out.print(" ");
+         }
+
+         for(int j=0;j<n-i;j++){
+            System.out.print("*");
+         }
+         System.out.println("");
+      }
+
+      for(int i=0;i<n;i++){
+
+         for(int j=0;j<i+1;j++){
+            System.out.print("*");
+         }
+
+         for(int k=0;k<2*(n-1-i);k++){
+            System.out.print(" ");
+         }
+
+         for(int j=0;j<i+1;j++){
+            System.out.print("*");
+         }
+
+         System.out.println("");
+
+      }
+   }
+}
