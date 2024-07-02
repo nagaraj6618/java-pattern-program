c569fa1b782979eeecefa881518cb3bfd8d21291
NAGARAJ S <nagaraj516700@gmail.com>
2024-07-02 11:51:20 +0530

    Pattern Added

---
+++ pattern12.java
@@ -0,0 +1,39 @@
+public class pattern12 {
+   public static void main(String[] args) {
+       
+      int n = 5;
+
+      for(int i=0;i<n;i++){
+
+
+         for( int j=0;j<i+1;j++){
+            System.out.print("*");
+         }
+         for(int k=0;k<2*(n-i-1);k++){
+            System.out.print(" ");
+         }
+
+         for(int j=0;j<i+1;j++){
+            System.out.print("*");
+         }
+         System.out.print("\n");
+      }
+
+      for(int i=0;i<n;i++){
+
+         for(int j=0;j<n-i-1;j++){
+            System.out.print("*");
+         }
+
+         for(int k=0;k<2*(i+1);k++){
+            System.out.print(" ");
+         }
+
+         for(int j=0;j<n-i-1;j++){
+            System.out.print("*");
+         }
+
+         System.out.print("\n");
+      }
+   }
+}
