d526163b5aaa860f170146b1357107aa12a1b002
NAGARAJ S <nagaraj516700@gmail.com>
2024-07-02 11:37:38 +0530

    Pattern Added

--- pattern11.java
+++ pattern11.java
@@ -1,3 +1,18 @@
+/*
+      **********
+      ****  ****
+      ***    ***
+      **      **
+      *        *
+      *        *
+      **      **
+      ***    ***
+      ****  ****
+      **********
+ */
+
+
+
 public class pattern11 {
    public static void main(String[]args){
       int n = 5;
